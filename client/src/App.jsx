db7f5c72ef1240c530812ec322e3e82009b16a66
Daphne <71684146+duckysheep@users.noreply.github.com>
2022-12-12 16:26:31 +0800

    updated client with navbar and 'holidays'+'edit holidays' pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import EditHolidaysPage from "./pages/EditHolidaysPage";
+import HolidaysPage from "./pages/HolidaysPage";
+
 function App() {
   return (
     <>
-      <h1>Holidays</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/holidays" element={<HolidaysPage />} />
+          <Route path="/holidays/:id" element={<EditHolidaysPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
